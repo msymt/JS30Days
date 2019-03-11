5e4dc3e1f3f3ac1868bcace20c8e35c38f5e0475
Masaya <nam.nam580@gmail.com>
2019-03-11 23:01:12 +0900

    correspond to input from keyboard

--- drumKit/drumKit.js
+++ drumKit/drumKit.js
@@ -8,9 +8,17 @@ function playSound(soundName) {
   audio.currentTime = 0; // reset the play time
   audio.play();
 }
-// 次回
+
 window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key]='${e.keyCode}'`);
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key-box[data-key='${e.keyCode}']`);
+  if (!audio) return;
   audio.currentTime = 0; // reset the play time
   audio.play();
+  key.classList.add('playing'); // border-color: yellow
+});
+
+window.addEventListener('keyup', function(e) {
+  const key = document.querySelector(`.key-box[data-key='${e.keyCode}']`);
+  key.classList.remove('playing'); // remove the border-color
 });
