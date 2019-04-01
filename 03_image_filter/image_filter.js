938b9ab0eb78c9519b96a04ed267333209717d2e
Masaya <nam.nam580@gmail.com>
2019-04-02 02:15:10 +0900

    03 done

--- 03_image_filter/image_filter.js
+++ 03_image_filter/image_filter.js
@@ -1,28 +1,20 @@
-// document.getElementById('color-selected', function(color) {
-//   console.log(color);
-// });
+function colorChange(event) {
+  const imageContainerElement = document.querySelector('.image-container');
+  const currentColor = event.srcElement.value;
+  imageContainerElement.style.backgroundColor = currentColor;
+}
 
+function blurChange(event) {
+  const imageElement = document.querySelector('.image');
+  currentBlurValue = event.srcElement.value;
+  console.log(`blur(${currentBlurValue}`);
+  imageElement.style.filter = `blur(${currentBlurValue - 1}px)`;
+  imageElement.style.WebkitFilter = `blur(${currentBlurValue - 1}px)`;
+}
 
-// const test = document.getElementById('selected-color-section').value;
-// const test = document.getElementById('color-selected').value;
-// console.log(test);
-
-// const elem = document.getElementById('spacing-range');
-// const target = document.getElementById('value');
-
-// const rangeValue = function(elem, target) {
-//   return function(event) {
-//     target.innerHTML = elem.value;
-//   };
-// };
-// elem.addEventListener('input', rangeValue(elem, target));
-
-// const getColor = document.getElementById('color-selected');
-// console.log(getColor);
-
-colorPicker.addEventListener('input', updateFirst, false);
-colorPicker.addEventListener('change', watchColorPicker, false);
-
-function watchColorPicker(event) {
-  document.getElementById()
-};
+function spacingChange(event) {
+  const imageElement = document.querySelector('.image');
+  const currentSpace = event.srcElement.value;
+  imageElement.style.marginLeft = `${currentSpace}px`;
+  imageElement.style.marginTop = `${currentSpace}px`;
+}
